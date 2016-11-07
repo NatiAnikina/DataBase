3787c29b07eadef4d3075c7b7dd91989e9aeea4c
Михаил <@->
2016-11-07 15:49:56 +0300

    Я сделяль

--- StudentQueries/Context.cs
+++ StudentQueries/Context.cs
@@ -7,12 +7,12 @@ using System.Threading.Tasks;
 
 namespace StudentQueries
 {
-	class Context : DbContext
+	class RRRRRContext : DbContext
 	{
 		public DbSet<Student> Students { get; set; }
 		public DbSet<Group> Groups { get; set; }
 
-		public Context() : base("localsql")
+		public RRRRRContext() : base("localsql")
 		{
 
 		}
